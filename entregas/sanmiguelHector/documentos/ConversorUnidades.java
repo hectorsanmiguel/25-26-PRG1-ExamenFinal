9afea28d84ba0d674eba70693475357a7dbd3f45
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-01-09 15:35:37 +0100

    Modificada la variable "o" a "conversionElegida"

--- entregas/sanmiguelHector/documentos/ConversorUnidades.java
+++ entregas/sanmiguelHector/documentos/ConversorUnidades.java
@@ -22,21 +22,21 @@ public class ConversorUnidades {
             System.out.println("[5] Centimetros a Pulgadas");
             System.out.println("[6] Pulgadas a Centimetros");
             System.out.print("Opcion: ");
-            int o = scanner.nextInt();
+            int conversionElegida = scanner.nextInt();
             System.out.print("Valor: ");
             double v = scanner.nextDouble();
 
-            if (o == 1) {
+            if (conversionElegida == 1) {
                 System.out.println(v + " m = " + (v / 1000) + " km");
-            } else if (o == 2) {
+            } else if (conversionElegida == 2) {
                 System.out.println(v + " km = " + (v * 1000) + " m");
-            } else if (o == 3) {
+            } else if (conversionElegida == 3) {
                 System.out.println(v + " m = " + (v * 0.000621371) + " mi");
-            } else if (o == 4) {
+            } else if (conversionElegida == 4) {
                 System.out.println(v + " mi = " + (v * 1609.34) + " m");
-            } else if (o == 5) {
+            } else if (conversionElegida == 5) {
                 System.out.println(v + " cm = " + (v * 0.393701) + " in");
-            } else if (o == 6) {
+            } else if (conversionElegida == 6) {
                 System.out.println(v + " in = " + (v * 2.54) + " cm");
             } else {
                 System.out.println("Opcion no valida");
@@ -51,21 +51,21 @@ public class ConversorUnidades {
             System.out.println("[5] Gramos a Onzas");
             System.out.println("[6] Onzas a Gramos");
             System.out.print("Opcion: ");
-            int o = scanner.nextInt();
+            int conversionElegida = scanner.nextInt();
             System.out.print("Valor: ");
             double v = scanner.nextDouble();
 
-            if (o == 1) {
+            if (conversionElegida == 1) {
                 System.out.println(v + " kg = " + (v * 1000) + " g");
-            } else if (o == 2) {
+            } else if (conversionElegida == 2) {
                 System.out.println(v + " g = " + (v / 1000) + " kg");
-            } else if (o == 3) {
+            } else if (conversionElegida == 3) {
                 System.out.println(v + " kg = " + (v * 2.20462) + " lb");
-            } else if (o == 4) {
+            } else if (conversionElegida == 4) {
                 System.out.println(v + " lb = " + (v * 0.453592) + " kg");
-            } else if (o == 5) {
+            } else if (conversionElegida == 5) {
                 System.out.println(v + " g = " + (v * 0.035274) + " oz");
-            } else if (o == 6) {
+            } else if (conversionElegida == 6) {
                 System.out.println(v + " oz = " + (v * 28.3495) + " g");
             } else {
                 System.out.println("Opcion no valida");
@@ -78,17 +78,17 @@ public class ConversorUnidades {
             System.out.println("[3] Celsius a Kelvin");
             System.out.println("[4] Kelvin a Celsius");
             System.out.print("Opcion: ");
-            int o = scanner.nextInt();
+            int conversionElegida = scanner.nextInt();
             System.out.print("Valor: ");
             double v = scanner.nextDouble();
 
-            if (o == 1) {
+            if (conversionElegida == 1) {
                 System.out.println(v + " C = " + (v * 9 / 5 + 32) + " F");
-            } else if (o == 2) {
+            } else if (conversionElegida == 2) {
                 System.out.println(v + " F = " + ((v - 32) * 5 / 9) + " C");
-            } else if (o == 3) {
+            } else if (conversionElegida == 3) {
                 System.out.println(v + " C = " + (v + 273.15) + " K");
-            } else if (o == 4) {
+            } else if (conversionElegida == 4) {
                 System.out.println(v + " K = " + (v - 273.15) + " C");
             } else {
                 System.out.println("Opcion no valida");
